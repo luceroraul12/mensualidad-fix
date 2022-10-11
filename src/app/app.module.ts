7dcfd3929ba9284dfcc89db5dbd4ba6e02259e12
Homitowen <pycgames@gmail.com>
2022-10-11 00:38:24 -0300

    add flex layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppComponent } from './app.component';
 import { MensualidadModule } from './mensualidad/mensualidad.module';
 import { SharedModule } from './shared/shared.module';
@@ -13,6 +15,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FlexLayoutModule,
+
     MensualidadModule,
     SharedModule
   ],

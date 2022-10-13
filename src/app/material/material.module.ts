61a602686ab41bf9eade34c6fe7b85954ff87c92
Lucero Raul <luceroraul12@gmail.com>
2022-10-13 14:50:27 -0300

    agregando modules

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
-import {MatListModule} from '@angular/material/list';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInput, MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatNativeDateModule} from '@angular/material/core';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatTableModule} from '@angular/material/table';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 
@@ -24,19 +25,20 @@ import {MatCardModule} from '@angular/material/card';
     CommonModule,
   ],
   exports:[
-    MatSidenavModule,
     MatButtonModule,
-    MatListModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatTableModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
     MatFormFieldModule,
+    MatIconModule,
     MatInputModule,
+    MatListModule,
+    MatNativeDateModule,
     MatRadioModule,
     MatSelectModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatCardModule
+    MatSidenavModule,
+    MatTableModule,
+    MatToolbarModule,
   ]
 })
 export class MaterialModule { }

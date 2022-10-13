9a69759ab88b3e2789fb5c1ba4422298a1d62337
Lucero Raul <luceroraul12@gmail.com>
2022-10-13 13:39:59 -0300

    cambiando directorio de components

--- src/app/mensualidad/mensualidad-routing.module.ts
+++ src/app/mensualidad/mensualidad-routing.module.ts
@@ -1,10 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormularioPagoComponent } from './components/formulario-pago/formulario-pago.component';
-import { FormularioServicioComponent } from './components/formulario-servicio/formulario-servicio.component';
-import { TablaPagoComponent } from './components/tabla-pago/tabla-pago.component';
-import { TablaResumenComponent } from './components/tabla-resumen/tabla-resumen.component';
-import { TablaServicioComponent } from './components/tabla-servicio/tabla-servicio.component';
 import { MensualidadComponent } from './mensualidad.component';
 import { PagComponent } from './pag/pag.component';
 import { ResumenComponent } from './resumen/resumen.component';
--- src/app/mensualidad/mensualidad.module.ts
+++ src/app/mensualidad/mensualidad.module.ts
@@ -5,18 +5,18 @@ import { MensualidadRoutingModule } from './mensualidad-routing.module';
 import { MensualidadComponent } from './mensualidad.component';
 import { MaterialModule } from '../material/material.module';
 import { SharedModule } from '../shared/shared.module';
-import { FormularioPagoComponent } from './components/formulario-pago/formulario-pago.component';
 import { MenuComponent } from './menu/menu.component';
-import { FormularioServicioComponent } from './components/formulario-servicio/formulario-servicio.component';
-import { TablaPagoComponent } from './components/tabla-pago/tabla-pago.component';
-import { TablaResumenComponent } from './components/tabla-resumen/tabla-resumen.component';
-import { TablaServicioComponent } from './components/tabla-servicio/tabla-servicio.component';
+import { TablaPagoComponent } from './pag/components/tabla-pago/tabla-pago.component';
+import { TablaServicioComponent } from './servicio/components/tabla-servicio/tabla-servicio.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { ServicioComponent } from './servicio/servicio.component';
 import { PagComponent } from './pag/pag.component';
 import { ResumenComponent } from './resumen/resumen.component';
-import { FormularioResumenComponent } from './components/formulario-resumen/formulario-resumen.component';
+import { FormularioResumenComponent } from './resumen/components/formulario-resumen/formulario-resumen.component';
+import { FormularioPagoComponent } from './pag/components/formulario-pago/formulario-pago.component';
+import { TablaResumenComponent } from './resumen/components/tabla-resumen/tabla-resumen.component';
+import { FormularioServicioComponent } from './servicio/components/formulario-servicio/formulario-servicio.component';
 
 
 @NgModule({

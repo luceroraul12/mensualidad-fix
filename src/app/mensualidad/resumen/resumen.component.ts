be2dcf8aa8a5d5546f10880c04e8feeb02f33512
Lucero Raul <luceroraul12@gmail.com>
2022-11-08 12:59:39 -0300

    add implementacion de observable resumen de pagos

--- src/app/mensualidad/resumen/resumen.component.ts
+++ src/app/mensualidad/resumen/resumen.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Pago } from 'src/app/interfaces/pago.interface';
+import { Factura } from 'src/app/interfaces/servicio.interface';
 import { ComunicadorService } from 'src/app/services/comunicador.service';
+import { PagoService } from 'src/app/services/pago.service';
+import { ServicioService } from 'src/app/services/servicio.service';
 
 @Component({
   selector: 'app-resumen',
@@ -9,15 +13,29 @@ import { ComunicadorService } from 'src/app/services/comunicador.service';
 })
 export class ResumenComponent implements OnInit {
 
-  public fechaElegida: Date = new Date(Date.now());
+  public fechaElegida!: Date;;
+
+  public facturasPagadas!: Factura[];
+  public facturasSinPagar!: Factura[];
+  public pagosRealizados!: Pago[];
 
   constructor(
-    private comunicadorService: ComunicadorService
+    private comunicadorService: ComunicadorService,
+    private facturaService: ServicioService,
+    private pagoService: PagoService
   ) { }
 
   ngOnInit(): void {
+    this.fechaElegida = new Date(Date.now());
     this.comunicadorService.fechaResumen$.subscribe(
       fecha => this.fechaElegida = fecha
+    )
+    this.pagoService.obtenerResumen(this.fechaElegida.getMonth()+1,this.fechaElegida.getFullYear()).subscribe(
+      pagosRealizados => {
+        this.pagosRealizados = pagosRealizados;
+        console.log(pagosRealizados);
+        
+      }
     )
   }
 

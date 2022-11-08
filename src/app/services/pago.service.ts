7fe455e32686e302411cbb365a4a33dee1ed74c7
Lucero Raul <luceroraul12@gmail.com>
2022-11-08 12:59:05 -0300

    add metodo para obtener resumen de pagos

--- src/app/services/pago.service.ts
+++ src/app/services/pago.service.ts
@@ -1,4 +1,6 @@
+import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Pago } from '../interfaces/pago.interface';
 import { CrudService } from './crud.service';
 
@@ -7,5 +9,13 @@ import { CrudService } from './crud.service';
 })
 export class PagoService extends CrudService<Pago>{
   public rutaEspecifica: string = "pagos";
+
+  obtenerResumen(mes: number, anio: number): Observable<Pago[]> {
+    let params: HttpParams = new HttpParams()
+                      .set('mes',mes)
+                      .set('anio',anio);
+                      
+    return this.http.post<Pago[]>(`${this.url}/${this.rutaEspecifica}/resumen`, params)
+  }
 
 }

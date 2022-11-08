25f06e1da59d289466d78fa60bd64d6c3c341bad
Lucero Raul <luceroraul12@gmail.com>
2022-11-08 16:14:04 -0300

    add metodo de verificacion de facturas

--- src/app/services/resumen.service.ts
+++ src/app/services/resumen.service.ts
@@ -2,7 +2,9 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Pago } from '../interfaces/pago.interface';
 import { ResumenRespuesta } from '../interfaces/resumen-respuesta.interface';
+import { Factura } from '../interfaces/servicio.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +21,24 @@ export class ResumenService {
                                 .set("anio",anio);
 
     return this.http.get<ResumenRespuesta>(`${environment.urlApi}/resumen`,{params});
+  }
+
+ //////////////////////////////////////////////// //
+  verificarExistenciaFacturaAgregar(pago: Pago, facturas: Factura[]): Factura[]{
+    let resultado: Factura[];
+    let estaContenido: boolean = false;
+
+    facturas.forEach(({id}) => {
+      if(id == pago.factura.id){
+        estaContenido = true;
+      }
+    });
+
+    if(estaContenido){
+      resultado = facturas.filter( ({id}) => id != pago.factura.id);
+    } else {
+      resultado = [... facturas, pago.factura];
+    }
+    return resultado;
   }
 }

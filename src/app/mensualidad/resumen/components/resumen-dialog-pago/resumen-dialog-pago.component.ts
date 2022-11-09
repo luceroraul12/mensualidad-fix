9a913c4aa49a33bca8c97babc2d968504f1c96ff
Lucero Raul <luceroraul12@gmail.com>
2022-11-09 12:57:24 -0300

    add tabla service para usar observable y cerrar dialog

--- src/app/mensualidad/resumen/components/resumen-dialog-pago/resumen-dialog-pago.component.ts
+++ src/app/mensualidad/resumen/components/resumen-dialog-pago/resumen-dialog-pago.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Pago } from 'src/app/interfaces/pago.interface';
 import { Factura } from 'src/app/interfaces/servicio.interface';
+import { PagoService } from 'src/app/services/pago.service';
+import { TablaServiceService } from 'src/app/services/tabla-service.service';
 
 @Component({
   selector: 'app-resumen-dialog-pago',
@@ -12,11 +15,14 @@ export class ResumenDialogPagoComponent implements OnInit {
 
   constructor(
     private dialogRef: MatDialogRef<ResumenDialogPagoComponent>,
-    @Inject(MAT_DIALOG_DATA) public facturaSeleccionada: Factura 
+    @Inject(MAT_DIALOG_DATA) public facturaSeleccionada: Factura,
+    private tablaService: TablaServiceService<Pago>
   ) { }
 
   ngOnInit(): void {
-    
+    this.tablaService.comunicadorFormularioTabla$.subscribe(
+      pago => this.dialogRef.close()
+    )
   }
 
 

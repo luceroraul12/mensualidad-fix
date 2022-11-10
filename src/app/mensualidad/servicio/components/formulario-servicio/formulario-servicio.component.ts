f55d54b419da3e3bfc7ba1ee3d5f09146a61fcc1
Lucero Raul <luceroraul12@gmail.com>
2022-11-10 18:50:59 -0300

    add unsubscripbe ?

--- src/app/mensualidad/servicio/components/formulario-servicio/formulario-servicio.component.ts
+++ src/app/mensualidad/servicio/components/formulario-servicio/formulario-servicio.component.ts
@@ -31,7 +31,7 @@ export class FormularioServicioComponent implements OnInit, OnDestroy {
 
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.subscription?.unsubscribe();
   }
 
   ngOnInit(): void {

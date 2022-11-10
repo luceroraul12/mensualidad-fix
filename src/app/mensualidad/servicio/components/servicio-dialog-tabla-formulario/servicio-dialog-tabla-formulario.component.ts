6a2e5052a97c5f62adc80258d983969f32790721
Lucero Raul <luceroraul12@gmail.com>
2022-11-10 18:45:49 -0300

    add subscription y uso de tablaService

--- src/app/mensualidad/servicio/components/servicio-dialog-tabla-formulario/servicio-dialog-tabla-formulario.component.ts
+++ src/app/mensualidad/servicio/components/servicio-dialog-tabla-formulario/servicio-dialog-tabla-formulario.component.ts
@@ -1,20 +1,32 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
 import { Factura } from 'src/app/interfaces/servicio.interface';
+import { ComunicadorService } from 'src/app/services/comunicador.service';
+import { TablaServiceService } from 'src/app/services/tabla-service.service';
 
 @Component({
   selector: 'app-servicio-dialog-tabla-formulario',
   templateUrl: './servicio-dialog-tabla-formulario.component.html',
   styleUrls: ['./servicio-dialog-tabla-formulario.component.css']
 })
-export class ServicioDialogTablaFormularioComponent implements OnInit {
+export class ServicioDialogTablaFormularioComponent implements OnInit, OnDestroy {
+
+  private subscription!: Subscription;
 
   constructor(
     public dialogRef: MatDialogRef<ServicioDialogTablaFormularioComponent>,
-    @Inject(MAT_DIALOG_DATA) public factura: Factura
+    @Inject(MAT_DIALOG_DATA) public factura: Factura,
+    public tablaService: TablaServiceService<Factura>
   ) { }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   ngOnInit(): void {
+    this.subscription = this.tablaService.comunicadorFormularioTabla$.subscribe(
+      factura => this.dialogRef.close()
+    );
   }
 
 }

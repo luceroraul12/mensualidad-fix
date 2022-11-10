a9ce70fa411eee84d9aba86327fde58cbddf28df
Lucero Raul <luceroraul12@gmail.com>
2022-11-10 12:34:42 -0300

    add dialog y servicio injenct

--- src/app/mensualidad/servicio/components/servicio-dialog-tabla-formulario/servicio-dialog-tabla-formulario.component.ts
+++ src/app/mensualidad/servicio/components/servicio-dialog-tabla-formulario/servicio-dialog-tabla-formulario.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Factura } from 'src/app/interfaces/servicio.interface';
 
 @Component({
   selector: 'app-servicio-dialog-tabla-formulario',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicioDialogTablaFormularioComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<ServicioDialogTablaFormularioComponent>,
+    @Inject(MAT_DIALOG_DATA) public factura: Factura
+  ) { }
 
   ngOnInit(): void {
   }

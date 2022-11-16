4b8b15ae2c30c854aaab2d15634fbad33a26b52d
Lucero Raul <luceroraul12@gmail.com>
2022-11-16 11:35:22 -0300

    change cuando existe un cabmio en resumen, traigo todo el resumen del servidor nuevamente.

--- src/app/mensualidad/resumen/components/tabla-resumen/tabla-resumen.component.ts
+++ src/app/mensualidad/resumen/components/tabla-resumen/tabla-resumen.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { first } from 'rxjs';
+import { first, Subscription } from 'rxjs';
 import { Pago } from 'src/app/interfaces/pago.interface';
 import { Factura } from 'src/app/interfaces/servicio.interface';
 import { ComunicadorService } from 'src/app/services/comunicador.service';
 import { PagoService } from 'src/app/services/pago.service';
 import { ResumenService } from 'src/app/services/resumen.service';
+import { ServicioService } from 'src/app/services/servicio.service';
 import { TablaServiceService } from 'src/app/services/tabla-service.service';
 import { ResumenDialogPagoComponent } from '../resumen-dialog-pago/resumen-dialog-pago.component';
 
@@ -15,42 +16,43 @@ import { ResumenDialogPagoComponent } from '../resumen-dialog-pago/resumen-dialo
   styles: [
   ]
 })
-export class TablaResumenComponent implements OnInit {
+export class TablaResumenComponent implements OnInit, OnDestroy {
 
   public fechaElegida!: Date;;
 
   public facturasPagadas!: Factura[];
   public facturasSinPagar!: Factura[];
   public pagosRealizados!: Pago[];
+  private facturasTotales!: Factura[];
+  private subscriptions: Subscription = new Subscription();
 
   constructor(
     private comunicadorService: ComunicadorService,
     private resumenService: ResumenService,
     private tablaService: TablaServiceService<Pago>,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private facturaService: ServicioService
   ) { }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.fechaElegida = new Date(Date.now());
-    this.comunicadorService.fechaResumen$.subscribe(
+    this.subscriptions.add(this.comunicadorService.fechaResumen$.subscribe(
       fecha => {
         this.fechaElegida = fecha;
         this.traerTodosLosPagosDelBackend();
       }
-    )
-    this.tablaService.comunicadorFormularioTabla$.subscribe(
-      ({elemento}) => {
-        this.recuperarFacturaPagada(elemento);
+    ));
+    this.subscriptions.add(this.tablaService.comunicadorFormularioTabla$.subscribe(
+      () => {
+        this.traerTodosLosPagosDelBackend();
       } 
-    )
+    ));
+    this.subscriptions.add(this.facturaService.leer().subscribe(resultado => this.facturasTotales = resultado));
   }
-
-  recuperarFacturaPagada(pago: Pago):void{
-    console.log(pago);
-    this.facturasPagadas = this.resumenService.verificarExistenciaFacturaAgregar(pago, this.facturasPagadas);
-    this.facturasSinPagar = this.resumenService.verificarExistenciaFacturaAgregar(pago, this.facturasSinPagar);
-  }
-
   abrirFromularioPago(facturaSeleccionada: Factura): void {
     this.dialog.open(ResumenDialogPagoComponent,{data: facturaSeleccionada})
   }

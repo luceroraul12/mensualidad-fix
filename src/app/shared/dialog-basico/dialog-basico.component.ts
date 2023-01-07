1dcada836a8eecbe94a6246cb9df8a9526a2cad6
Lucero Raul <luceroraul12@gmail.com>
2023-01-07 17:44:04 -0300

    add dialog basico component

---
+++ src/app/shared/dialog-basico/dialog-basico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-basico',
+  templateUrl: './dialog-basico.component.html',
+  styleUrls: ['./dialog-basico.component.css']
+})
+export class DialogBasicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

e5c3825a4edccc7a77e5dda29983cbf3a6245122
Lucero Raul <luceroraul12@gmail.com>
2023-01-07 17:44:07 -0300

    add atr titulo subtitulo

--- src/app/shared/dialog-basico/dialog-basico.component.ts
+++ src/app/shared/dialog-basico/dialog-basico.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dialog-basico',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dialog-basico.component.css']
 })
 export class DialogBasicoComponent implements OnInit {
+
+  @Input()  titulo: string= "";
+  @Input()  subtitulo: string= "";
 
   constructor() { }
 

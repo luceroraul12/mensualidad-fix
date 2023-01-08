2fa095f8637f31d4acce0bff3292ebbb79abab2e
Lucero Raul <luceroraul12@gmail.com>
2023-01-08 09:02:45 -0300

    add ngcontent y input titulo

--- src/app/shared/vista/vista.component.ts
+++ src/app/shared/vista/vista.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-vista',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vista.component.css']
 })
 export class VistaComponent implements OnInit {
+
+  @Input() titulo!: string;
 
   constructor() { }
 

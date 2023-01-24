1315e5cc5b9e19afec374720fec97d62a6d02015
Lucero Raul <luceroraul12@gmail.com>
2023-01-24 00:50:20 -0300

    add tabla de usuarios component

--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { UsuarioComponent } from './usuario.component';
 import { MaterialModule } from '../material/material.module';
 import { SharedModule } from '../shared/shared.module';
+import { TablaUsuarioComponent } from './component/tabla-usuario/tabla-usuario.component';
 
 
 
 @NgModule({
   declarations: [
-    UsuarioComponent
+    UsuarioComponent,
+    TablaUsuarioComponent
   ],
   imports: [
     CommonModule,

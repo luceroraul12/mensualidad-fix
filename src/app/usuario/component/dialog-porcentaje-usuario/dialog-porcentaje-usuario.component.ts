bd1ce149ef1edb1ecd507545d532e30abb77fdd9
Lucero Raul <luceroraul12@gmail.com>
2023-01-24 23:29:22 -0300

    add dialog para porcentaje usuarios

--- src/app/usuario/component/dialog-porcentaje-usuario/dialog-porcentaje-usuario.component.ts
+++ src/app/usuario/component/dialog-porcentaje-usuario/dialog-porcentaje-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LegendPosition } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-dialog-porcentaje-usuario',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogPorcentajeUsuarioComponent implements OnInit {
 
+  single!: any[];
+  view: [number, number] = [700, 400];
+
+  // options
+  gradient: boolean = false;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  legendPosition: LegendPosition = LegendPosition.Below;
+
+  colorScheme =  "nightLights";
+
+  public data: any = [
+  {
+    "name": "Raul",
+    "value": 60,
+    "extra": {
+      "code": "Homito"
+    }
+  },
+  {
+    "name": "Homito",
+    "value": 40,
+    "extra": {
+      "code": "Mgs"
+    }
+  },
+  {
+    "name": "Shoverto",
+    "value": 90,
+    "extra": {
+      "code": "Shover"
+    }
+  }
+]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
 }
--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -5,6 +5,8 @@ import { MaterialModule } from '../material/material.module';
 import { SharedModule } from '../shared/shared.module';
 import { TablaUsuarioComponent } from './component/tabla-usuario/tabla-usuario.component';
 import { DialogPorcentajeUsuarioComponent } from './component/dialog-porcentaje-usuario/dialog-porcentaje-usuario.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
@@ -17,7 +19,9 @@ import { DialogPorcentajeUsuarioComponent } from './component/dialog-porcentaje-
   imports: [
     CommonModule,
     MaterialModule,
-    SharedModule
+    SharedModule,
+    NgxChartsModule,
+    BrowserAnimationsModule
   ]
 })
 export class UsuarioModule { }

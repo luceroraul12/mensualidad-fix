32a80948fd65f5c74dede5b4bb5505ffb30ab8a7
Lucero Raul <luceroraul12@gmail.com>
2023-01-26 00:21:21 -0300

    add eventos en deslizador

--- src/app/usuario/component/deslizador-usuario/deslizador-usuario.component.ts
+++ src/app/usuario/component/deslizador-usuario/deslizador-usuario.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { UsuarioDto } from 'src/app/interfaces/usuarioDto.interface';
+import { UsuarioComunicacionService } from 'src/app/services/usuario-comunicacion.service';
 
 @Component({
   selector: 'app-deslizador-usuario',
@@ -8,10 +9,16 @@ import { UsuarioDto } from 'src/app/interfaces/usuarioDto.interface';
 })
 export class DeslizadorUsuarioComponent implements OnInit {
   @Input() usuario!: UsuarioDto;
+  @Input() porcentaje!: number;
 
-  constructor() { }
+  constructor(
+    private usuarioComunicadorService: UsuarioComunicacionService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  actualizar(): void {
+    this.usuarioComunicadorService.comunicador$.next(this.usuario);
+  }
 }

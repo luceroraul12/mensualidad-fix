802200c9275ba66c301dc2cf207d097dcbb6205e
Lucero Raul <luceroraul12@gmail.com>
2023-01-25 21:51:05 -0300

    add service para porcentaje

--- src/app/services/porcentaje-usuarios-facturas.service.ts
+++ src/app/services/porcentaje-usuarios-facturas.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PorcentajeUsuariosFacturas } from '../interfaces/porcentaje-usuarios-facturas.interface';
+import { CrudService } from './crud.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PorcentajeUsuariosFacturasService {
-
-  constructor() { }
+export class PorcentajeUsuariosFacturasService extends CrudService<PorcentajeUsuariosFacturas> {
+  public override rutaEspecifica: string = "porcentaje-usuarios-facturas";
 }
